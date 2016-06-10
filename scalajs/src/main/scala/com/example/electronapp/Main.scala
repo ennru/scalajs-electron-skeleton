127ac347e1e8878c4e67d72a1a20f56c4a366f3c
Enno Runne <enno.runne@baymarkets.com>
2016-06-10 14:38:18 +0200

    Copied Electron quick start JS code even more

--- scalajs/src/main/scala/com/example/electronapp/Main.scala
+++ scalajs/src/main/scala/com/example/electronapp/Main.scala
@@ -9,30 +9,19 @@ object Main extends js.JSApp {
 
   def main(): Unit = {
 
-    val electron = g.require("electron");
+    val electron = g.require("electron")
     val app = electron.app.asInstanceOf[App] // Module to control application life.
 
     // Keep a global reference of the window object, if you don't, the window will
     // be closed automatically when the JavaScript object is GCed.
-    var mainWindow: BrowserWindow = null;
+    var mainWindow: BrowserWindow = null
 
-    // Quit when all windows are closed.
-    app.on("window-all-closed", { () =>
-      // On OS X it is common for applications and their menu bar
-      // to stay active until the user quits explicitly with Cmd + Q
-      if (Process.platform != "darwin") {
-        app.quit()
-      }
-    });
-
-    // This method will be called when Electron has finished
-    // initialization and is ready to create browser windows.
-    app.on("ready", () => {
+    def createWindow(): Unit = {
       // Create the browser window.
       mainWindow = BrowserWindow(width = 800, height = 600)
 
       // and load the index.html of the app.
-      mainWindow.loadURL("file://" + g.__dirname + "/index.html")
+      mainWindow.loadURL(s"file://${g.__dirname}/index.html")
 
       // Open the devtools.
       mainWindow.openDevTools()
@@ -43,9 +32,28 @@ object Main extends js.JSApp {
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null
-      );
+      )
+    }
 
-    });
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    app.on("ready", () => createWindow())
 
+    // Quit when all windows are closed.
+    app.on("window-all-closed", { () =>
+      // On OS X it is common for applications and their menu bar
+      // to stay active until the user quits explicitly with Cmd + Q
+      if (Process.platform != "darwin") {
+        app.quit()
+      }
+    })
+
+    app.on("activate", () => {
+      // On OS X it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (mainWindow == null) {
+        createWindow()
+      }
+    })
   }
 }
